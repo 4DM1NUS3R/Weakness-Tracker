d8c0dca199ae88d4e7509ec30559543aca3667d3
Quentin <quentin.chabenat@efrei.net>
2021-06-29 16:12:54 +0200

    bug de ses morts réglé

--- Slack-back/app.py
+++ Slack-back/app.py
@@ -1,16 +1,16 @@
-from flask import Flask
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'hello world'
 
 
 @app.route('/test')
-def hello_world():
-    return Flask.jsonify({'test': 'Hello World!'})
-
-
-@app.route('/WeaknessTracker/dns/basic')
-def hello_world():
-    return Flask.jsonify({'test': 'Hello World!'})
+def test():
+    return flask.jsonify({'test': 'Hello World!'})
 
 
 if __name__ == '__main__':
