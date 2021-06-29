ffde945f4208de23ece8548aee174dfe17baef06
Walidichoe <65547429+Walidichoe@users.noreply.github.com>
2021-06-29 15:07:24 +0200

    annulation1

--- Slack-back/app.py
+++ Slack-back/app.py
@@ -6,11 +6,14 @@ app = Flask(__name__)
 @app.route('/test')
 def hello_world():
     return Flask.jsonify({'test': 'Hello World!'})
+<<<<<<< HEAD
 
 
 @app.route('/WeaknessTracker/dns/basic')
 def hello_world():
     return Flask.jsonify({'test': 'Hello World!'})
+=======
+>>>>>>> parent of 2af96b2 (jsondrdt)
 
 
 if __name__ == '__main__':
--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import About from './Pages/About';
 import Contact from './Pages/Contact';
 import Navigation from './Pages/Navigation';
 import Prevention from './Pages/Prevention';
-import Analyse from './Pages/analyse';
 
 import { BrowserRouter as Router, Route} from 'react-router-dom';
 
@@ -22,7 +21,6 @@ function App() {
         <Route path="/Prevention" exact component={Prevention}/>
         <Route path="/About" exact component={About} />
         <Route path="/Contact" exact component={Contact} />
-        <Route path="/analyse" exact component={Analyse} />
         
 
 
